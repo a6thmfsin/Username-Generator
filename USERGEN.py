b122e644dd38dcc752d76c9484ac7021ecfad986
greyhound <63813294+a6thmfsin@users.noreply.github.com>
2022-09-13 02:29:08 +0300

    Update USERGEN.py

--- USERGEN.py
+++ USERGEN.py
@@ -3,17 +3,17 @@
 import random
 import string
 
-with open('cirt-default-usernames.txt', 'r') as infile:
-    nouns = infile.read().strip(' \n').split('\n')
+with open('names.txt', 'r') as infile:
+    list1 = infile.read().strip(' \n').split('\n')
 with open('names1.txt', 'r') as infile:
-    adjectives = infile.read().strip(' \n').split('\n')
-with open('top-usernames-shortlist.txt','r') as inline:
-    censored = inline.read().strip(' \n').split('\n')
+    list2 = infile.read().strip(' \n').split('\n')
+#with open('top-usernames.txt','r') as inline:
+#    censored = inline.read().strip(' \n').split('\n')
 for count in range(1, 51):
-    word1 = random.choice(adjectives)
-    word2 = random.choice(nouns)
-    word1 =word1.title()
-    word2 =word2.title()
+    word1 = random.choice(list1)
+    word2 = random.choice(list2)
+    word1 = word1.title()
+    word2 = word2.title()
     username = '{}{}'.format(word1, word2)
 
     print("username" + str(count) + " = " + username)
